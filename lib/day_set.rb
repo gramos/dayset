c6af0340e8fe9be12d41380eb556d5c4eaec76b3
Gaston Ramos <ramos.gaston@gmail.com>
2012-12-26 10:53:11 -0300

    refactor added method gen_range

--- lib/day_set.rb
+++ lib/day_set.rb
@@ -8,10 +8,13 @@ class DaySet
   end
 
   def self.gen(args)
-    last_day = last_day_of_the_month args[:date].year, args[:date].month
-    low_limit  = Date.new(args[:date].year, args[:date].month, 1)
-    high_limit = Date.new(args[:date].year, args[:date].month, last_day)
-    array_days = (low_limit..high_limit)
-    array_days.each_slice(args[:group_number]).to_a
+    gen_range(args[:date]).each_slice(args[:group_number]).to_a
+  end
+
+  def self.gen_range(date)
+    last_day   = last_day_of_the_month date.year, date.month
+    low_limit  = Date.new(date.year, date.month, 1)
+    high_limit = Date.new(date.year, date.month, last_day)
+    (low_limit..high_limit)
   end
 end
