f559491bc266b60a053d4ffd2e1bd1bc91ff0a4b
Gaston Ramos <ramos.gaston@gmail.com>
2012-12-26 10:19:25 -0300

    first init commit

---
+++ lib/week_period.rb
@@ -0,0 +1,17 @@
+class WeekPeriod
+
+  def self.last_day_of_the_month(yyyy, mm)
+    d = Date.new yyyy, mm
+    d += 42
+    last_d = Date.new(d.year, d.month) - 1
+    last_d.day
+  end
+
+  def self.get(args)
+    last_day = last_day_of_the_month args[:date].year, args[:date].month
+    low_limit  = Date.new(args[:date].year, args[:date].month, 1)
+    high_limit = Date.new(args[:date].year, args[:date].month, last_day)
+    array_days = (low_limit..high_limit)
+    array_days.each_slice(args[:group_number]).to_a
+  end
+end
---
+++ spec/weekperiod_spec.rb
@@ -0,0 +1,27 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'week_period'
+require 'date'
+
+describe WeekPeriod do
+
+  it "should get periods grouped in set of 7 days" do
+    @date = Date.new(2012, 1)
+    @expected = (Date.new(2012, 1, 1)..Date.new(2012, 1, 7)).to_a
+    @args = { :date => @date, :group_number => 7 }
+
+    WeekPeriod.get(@args)[0].must_equal @expected
+  end
+
+  # it "should get the second set of 7 days" do
+  #   @date = Date.new(2012, 1)
+  #   @expected = Date.new(2012, 1, 8)..Date.new(2012, 1, 14)
+  #   WeekPeriod.get(@date, 2).must_equal @expected
+  # end
+
+  # It "should get the last set of 7 days" do
+  #   @date     = Date.new(2012, 1)
+  #   @expected = Date.new(2012, 1, 29)..Date.new(2012, 1, 31)
+  #   WeekPeriod.get(@date, 4).must_equal @expected
+  # end
+end
