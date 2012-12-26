7c83e1cce216b01486305cf8cc9b78e1dec62300
Gaston Ramos <ramos.gaston@gmail.com>
2012-12-26 10:29:01 -0300

    added spec "should get the second set of 7 days"

--- spec/day_set_spec.rb
+++ spec/day_set_spec.rb
@@ -1,23 +1,24 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require 'week_period'
+require 'day_set'
 require 'date'
 
-describe DaySet do
+describe "DaySet group in set of 7 days" do
 
-  it "should get periods grouped in set of 7 days" do
+  before do
     @date = Date.new(2012, 1)
-    @expected = (Date.new(2012, 1, 1)..Date.new(2012, 1, 7)).to_a
     @args = { :date => @date, :group_number => 7 }
-
-    DaySet.get(@args)[0].must_equal @expected
   end
 
-  # it "should get the second set of 7 days" do
-  #   @date = Date.new(2012, 1)
-  #   @expected = Date.new(2012, 1, 8)..Date.new(2012, 1, 14)
-  #   WeekPeriod.get(@date, 2).must_equal @expected
-  # end
+  it "should get first set of 7 days" do
+    expected = (Date.new(2012, 1, 1)..Date.new(2012, 1, 7)).to_a
+    DaySet.get(@args)[0].must_equal expected
+  end
+
+  it "should get the second set of 7 days" do
+    expected = (Date.new(2012, 1, 8)..Date.new(2012, 1, 14)).to_a
+    DaySet.get(@args)[1].must_equal expected
+  end
 
   # It "should get the last set of 7 days" do
   #   @date     = Date.new(2012, 1)
